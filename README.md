206f187ef9424bb4f4cc3d4680ca9c1a1cbfd27b
Pedro Lopez <plopez@poweranalytics.com>
2022-02-04 18:59:49 -0300

    Rectangles

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# Run the game
+
+1.  install python 3.x
+2.  install pygame : `pip install pygame`
+3.  run the game: python app.py
+
 ## Most commmon event types (from the documentation)
 
 - QUIT
--- app.py
+++ app.py
@@ -11,8 +11,11 @@ sky_surface = pygame.image.load("graphics/Sky.png").convert()
 ground_surface = pygame.image.load("graphics/ground.png").convert()
 text_surface = test_font.render("My game", False, "Black")
 
-snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
-snail_x_pos = 600
+snail_surf = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
+snail_rect = snail_surf.get_rect(bottomright=(800, 300))
+
+player_surf = pygame.image.load("graphics/player/player_walk_1.png").convert_alpha()
+player_rect = player_surf.get_rect(midbottom=(80, 300))
 
 while True:
     for event in pygame.event.get():
@@ -23,9 +26,10 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
-    snail_x_pos -= 4
-    if snail_x_pos <= -100:
-        snail_x_pos = 800
-    screen.blit(snail_surface, (snail_x_pos, 265))
+    snail_rect.x -= 4
+    if snail_rect.right <= 0:
+        snail_rect.left = 800
+    screen.blit(snail_surf, snail_rect)
+    screen.blit(player_surf, player_rect)
     pygame.display.update()
     clock.tick(60)
