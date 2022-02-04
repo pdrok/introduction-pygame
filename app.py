29919461d87189e838334dd0a53c19a6251a13f3
Pedro Lopez <plopez@poweranalytics.com>
2022-02-04 17:32:38 -0300

    basic animation

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
-sky_surface = pygame.image.load("graphics/Sky.png")
-ground_surface = pygame.image.load("graphics/ground.png")
+sky_surface = pygame.image.load("graphics/Sky.png").convert()
+ground_surface = pygame.image.load("graphics/ground.png").convert()
 text_surface = test_font.render("My game", False, "Black")
+
+snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
+snail_x_pos = 600
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -19,5 +23,9 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
+    snail_x_pos -= 4
+    if snail_x_pos <= -100:
+        snail_x_pos = 800
+    screen.blit(snail_surface, (snail_x_pos, 265))
     pygame.display.update()
     clock.tick(60)
