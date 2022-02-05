dea3084b0296ed31908979f590040cfb10a9e960
Pedro Lopez <cde.luis@gmail.com>
2022-02-05 15:13:25 -0300

    Displaying the score, measuring time

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 import pygame
 from sys import exit
 
+def display_score():
+    current_time = int(pygame.time.get_ticks() / 1000 ) - start_time
+    score_surf = test_font.render(f'Score: {current_time}', False, (64, 64, 64))
+    score_rect = score_surf.get_rect(center = (400, 50))
+    screen.blit(score_surf, score_rect)
+
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 game_active = True 
+start_time = 0
 
 sky_surface = pygame.image.load("graphics/Sky.png").convert()
 ground_surface = pygame.image.load("graphics/ground.png").convert()
 
-score_surf = test_font.render("My game", False, (64, 64, 64))
-score_rect = score_surf.get_rect(center=(400, 50))
+# score_surf = test_font.render("My game", False, (64, 64, 64))
+# score_rect = score_surf.get_rect(center=(400, 50))
 
 snail_surf = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
 snail_rect = snail_surf.get_rect(bottomright=(800, 300))
@@ -36,12 +43,14 @@ while True:
         else:
             game_active = True
             snail_rect.left = 800
+            start_time = int(pygame.time.get_ticks() / 1000)
     if game_active:
         screen.blit(sky_surface, (0, 0))
         screen.blit(ground_surface, (0, 300))
-        pygame.draw.rect(screen, "#c0e8ec", score_rect)
-        pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
-        screen.blit(score_surf, score_rect)
+        # pygame.draw.rect(screen, "#c0e8ec", score_rect)
+        # pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
+        # screen.blit(score_surf, score_rect)
+        display_score()
 
         snail_rect.x -= 4
         if snail_rect.right <= 0:
