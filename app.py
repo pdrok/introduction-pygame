c284d4b939fec4e5481d8fb1e5b92810e9be7e0c
Pedro Lopez <plopez@poweranalytics.com>
2022-02-05 13:01:29 -0300

    The player Character 2: Jumping, Gravity & Creating floor

--- app.py
+++ app.py
@@ -18,17 +18,19 @@ snail_rect = snail_surf.get_rect(bottomright=(800, 300))
 
 player_surf = pygame.image.load("graphics/player/player_walk_1.png").convert_alpha()
 player_rect = player_surf.get_rect(midbottom=(80, 300))
+player_gravity = 0
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
+                player_gravity = -20
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                print("jump")
-        if event.type == pygame.KEYUP:
-            print("key up")
+            if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                player_gravity = -20
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
@@ -41,17 +43,14 @@ while True:
         snail_rect.left = 800
 
     screen.blit(snail_surf, snail_rect)
+
+    # Player
+    player_gravity += 1
+    player_rect.y += player_gravity
+    if player_rect.bottom >= 300:
+        player_rect.bottom = 300
     screen.blit(player_surf, player_rect)
-
-    # keys = pygame.key.get_pressed()
-    # if keys[pygame.K_SPACE]:
-    #     print("jump")
-    # if player_rect.colliderect(snail_rect):
-    #     print("collision")
-
-    # mouse_pos = pygame.mouse.get_pos()
-    # if player_rect.collidepoint(mouse_pos):
-    #     print("collision")
+    screen.blit(player_surf, player_rect)
 
     pygame.display.update()
     clock.tick(60)
