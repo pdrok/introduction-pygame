5fe44edc6d9775516aad53099fc7c09b903c8fc7
Pedro Lopez <plopez@poweranalytics.com>
2022-02-05 12:23:21 -0300

    Drawing with rectangles + colors

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
 sky_surface = pygame.image.load("graphics/Sky.png").convert()
 ground_surface = pygame.image.load("graphics/ground.png").convert()
-text_surface = test_font.render("My game", False, "Black")
+
+score_surf = test_font.render("My game", False, (64, 64, 64))
+score_rect = score_surf.get_rect(center=(400, 50))
 
 snail_surf = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
 snail_rect = snail_surf.get_rect(bottomright=(800, 300))
@@ -25,11 +27,23 @@ while True:
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (300, 50))
+    pygame.draw.rect(screen, "#c0e8ec", score_rect)
+    pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
+    screen.blit(score_surf, score_rect)
+
     snail_rect.x -= 4
     if snail_rect.right <= 0:
         snail_rect.left = 800
+
     screen.blit(snail_surf, snail_rect)
     screen.blit(player_surf, player_rect)
+
+    # if player_rect.colliderect(snail_rect):
+    #     print("collision")
+
+    mouse_pos = pygame.mouse.get_pos()
+    if player_rect.collidepoint(mouse_pos):
+        print("collision")
+
     pygame.display.update()
     clock.tick(60)
